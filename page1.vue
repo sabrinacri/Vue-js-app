a27843515c544297f7f12122c0b332bbfabb76d2
Sabrine Edded <sabrineedded@gmail.com>
2018-12-13 19:24:53 +0100

    Add files via upload

---
+++ page1.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="hello">
+        <h1>{{ msg }}</h1>
+        <router-link :to="{ name: 'Page2' }">Navigate to Page2</router-link>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Page1',
+        data () {
+            return {
+                msg: 'Welcome to Your Vue.js App'
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    h1, h2 {
+        font-weight: normal;
+    }
+
+    ul {
+        list-style-type: none;
+        padding: 0;
+    }
+
+    li {
+        display: inline-block;
+        margin: 0 10px;
+    }
+
+    a {
+        color: #42b983;
+    }
+</style>
